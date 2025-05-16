2d5e96cc8ba079b1685a692d62ecfb917c5074f1
prateeksingh74 <sing1542@purdue.edu>
2025-05-16 09:57:28 -0500

    remove element solution

--- easy/27-remove-element.c
+++ easy/27-remove-element.c
@@ -1,15 +1,20 @@
 /*
- * TODO - in progress
  * 27 - Remove Element - Easy
  * https://leetcode.com/problems/remove-element/description/?envType=study-plan-v2&envId=top-interview-150
  */
 
 /*
- * Solution explanation. Complexity analysis
+ * I have a counter for how many nums do not equal to value and I traverse throught the array and I count them.
+ * If the number does not equal to value, I first copy its value to the front of the array and then increment the counter.
  */
 
-int removeElement(int *nums, int numsSize, int val) {
-
-
-
+int removeElement(int* nums, int numsSize, int val) {
+    int k = 0;
+    for (int i = 0; i < numsSize; i++) {
+        if (nums[i] != val) {
+            nums[k] = nums[i];
+            k++; 
+        }
+    }
+    return k;
 }
