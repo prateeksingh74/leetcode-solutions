b402187348b3253fd07808a1f995ded28b3f42e4
prateeksingh74 <sing1542@purdue.edu>
2025-05-21 09:11:30 -0500

    problem 45 solution

--- medium/45-jump-game-2.c
+++ medium/45-jump-game-2.c
@@ -1,11 +1,32 @@
 /*
- * TODO
  * 45 - Jump Game 2 - Medium
- * link
+ * https://leetcode.com/problems/jump-game-ii/?envType=study-plan-v2&envId=top-interview-150
  */
 
 /*
- * description
+ * This solution calculates the minimum number of jumps needed to reach the end of an array, 
+ * where each element represents the maximum jump length from that position. It uses a greedy
+ * approach to track the farthest reachable index in the current jump range, and increments 
+ * the jump count whenever it reaches the end of that range. 
  */
+
+
+int jump(int* nums, int numsSize) {
+    int count = 0;
+    int currEnd = 0;
+    int farthest = 0;
+
+    for (int i = 0; i < numsSize - 1; i++) {
+        if (i + nums[i] > farthest) {
+            farthest = i + nums[i];
+        }
+
+        if (i == currEnd) {
+            count++;
+            currEnd = farthest;
+        }
+    }
+    return count;
+}
 
 
