eee92d3dadfa92403efa52c3c8150022c1198cc4
prateeksingh74 <sing1542@purdue.edu>
2025-07-09 01:00:31 -0500

    problem 217

---
+++ easy/217-valid-anagram.py
@@ -0,0 +1,14 @@
+/*
+ * 217 - Valid Anagram - Easy
+ * https://leetcode.com/problems/valid-anagram/description/
+ */
+
+/*
+ * Using sorted function in python and comparing the two strings.
+ */
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return false;
+        return sorted(s) == sorted(t)
