e0f08cd224dbf2bce8ad178a13ce3fa33db14bef
prateeksingh74 <sing1542@purdue.edu>
2026-01-29 16:41:47 -0500

    problem 274

--- medium/274-h-index.c
+++ medium/274-h-index.c
@@ -5,7 +5,17 @@
  */
 
 /*
- * description
+ * Works in linear time but since we reverse the list first, it is a little faster.
  */
 
-
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        citations.sort(reverse=True)
+        h = 0
+        for i, citation in enumerate(citations):
+            if citation >= i + 1:
+                h = i + 1
+            else:
+                break
+        
+        return h
